ee95737e7ab12f6691104fffd689e84847bfc95c
Josh Harcombe <joshharc@gmail.com>
2021-09-06 14:44:45 +1000

    new dotfiles

--- scripts/changebackground.sh
+++ scripts/changebackground.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-exit
 
 wal -q -i $1
 wal-discord
@@ -8,4 +7,11 @@ sed -i 's/@selected-.*fore/@back/' ~/.cache/wal/colors-rofi-dark.rasi
 
 off_white=`grep color7 ~/.cache/wal/colors.json | cut -d\" -f4`
 sed -i '/color15/s/: ".*"/: "'$off_white'"/' ~/.cache/wal/colors.json
-pywalfox update
+pywalfox update &
+
+# could probably do this much more easily by copying first x lines of file but whatever
+for change in `grep ": #" ~/.cache/wal/colors-rofi-dark.rasi | sed 's/ //g'`; do
+    field=`echo $change | cut -d# -f1`
+    color=`echo $change | cut -d# -f2`
+    sed -i "/ $field/s/#.*/#$color/" ~/.config/rofi/wal-theme.rasi
+done
