c3ed1c9ed6f2e4c1ff892c733a3c10e6bd6fd252
Josh Harcombe <joshharc@gmail.com>
2021-12-23 14:25:10 +1100

    new dotfiles

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,11 +3,12 @@ killall -q polybar &
 #polybar-msg cmd quit &
 wait
 # TODO: only launch one bar when screens are duplicated
+# TODO: if this is fixed change the polywins README example
 #while pgrep -x polybar >/dev/null; do sleep 2; done
 #polybar example & disown
 if type "xrandr"; then
   if [ `xrandr --query | grep " connected.*+0+0" | wc -l` -gt 1 ]; then
-    MONITOR=eDP-1 polybar example &
+    MONITOR=eDP-1 polybar --reload example &
     exit
   fi
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
