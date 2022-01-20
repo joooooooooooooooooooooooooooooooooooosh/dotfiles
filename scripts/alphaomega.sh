42d89f275cd81347fecdb0f0a6b89b67ee9b212b
Josh Harcombe <joshharc@gmail.com>
2022-01-20 12:12:06 +1100

    add ability to jump to first/last ws on monitor

---
+++ scripts/alphaomega.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z $1 ] || [ $1 != "tail" -a $1 != "head" ]; then
+    echo "Usage: $0 [tail|head]"
+    exit 1
+fi
+
+monitor=`i3-msg -t get_workspaces |
+    sed -E 's/output\":([^,]+),/output\1\n/g' |
+    grep \"focused\":true | 
+    sed 's/.*output\"//'`
+
+i3-msg workspace `i3-msg -t get_workspaces |
+    sed -E 's/output\":([^,]+),/\1\n/g' |
+    grep $monitor |
+    $1 -1 |
+    sed -E 's/.*num\":([^,]+),.*/\1/'`
