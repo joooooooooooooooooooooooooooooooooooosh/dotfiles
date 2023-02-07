0cfef50f8bb10eaa4353773ae45574fc2b05bf6e
Josh Harcombe <jharcombe@v60qdgww4x.office.atlassian.com>
2023-02-07 14:10:20 +1100

    add scripts

---
+++ scripts/updatedotfiles.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+DOT_DIR="$HOME/.dotfiles/"
+CONFIG_DIR=$DOT_DIR"config/"
+
+rm -r "${CONFIG_DIR}polybar/"
+cp -r ~/.config/polybar "${CONFIG_DIR}polybar/"
+rm -r "${DOT_DIR}scripts/"
+cp -r ~/Documents/scripts "$DOT_DIR"
+cp ~/.vimrc "${DOT_DIR}vim/"
+cp ~/.nvimrc "${DOT_DIR}vim/"
+cp ~/.vim/plugged/wal.vim/colors/wal.vim "${DOT_DIR}vim/"
+cp ~/.zshrc "$DOT_DIR"
+cp ~/.bashrc "$DOT_DIR"
+cp ~/.Xresources "$DOT_DIR"
+cp ~/.gitconfig "$DOT_DIR"
+cp ~/.gdbinit "$DOT_DIR"
+cp /etc/xdg/picom.conf "$DOT_DIR"
+cp ~/.config/fusuma/config.yml "${CONFIG_DIR}fusuma/"
+cp ~/.config/i3/config "${CONFIG_DIR}i3/"
+cp ~/.config/spicetify/config-xpui.ini "${CONFIG_DIR}spicetify/"
+cp -r ~/.config/spicetify/Themes/WalSleek "${CONFIG_DIR}spicetify/Themes/"
+cp  ~/.config/alacritty/alacritty.yml "${CONFIG_DIR}alacritty/"
+rm -rf "${CONFIG_DIR}ranger/"
+cp -r ~/.config/ranger "${CONFIG_DIR}ranger/"
+rm -r "${CONFIG_DIR}rofi/"
+cp -r ~/.config/rofi "${CONFIG_DIR}rofi/"
+rm -r "${DOT_DIR}vim/skeletons/"
+cp -r ~/.vim/skeletons "${DOT_DIR}vim/"
+rm -r "${CONFIG_DIR}wal/"
+cp -r ~/.config/wal "${CONFIG_DIR}wal/"
+rm -rf "${DOT_DIR}wal-discord/"
+cp -r ~/wal-discord "${DOT_DIR}wal-discord/"
+cp /var/spool/cron/joshh "${DOT_DIR}"
+cp /etc/udev/rules.d/80.power.rules "${DOT_DIR}"
+
+cur_theme=$(grep ^ZSH_THEME "${DOT_DIR}.zshrc" | cut -d\" -f2)
+
+[ "$cur_theme" != "random" ] && cp "$HOME/.oh-my-zsh/themes/$cur_theme.zsh-theme" "${DOT_DIR}current.zsh-theme"
+
+if ! { [ $# -ge 1 ] && [ "$1" == "-n" ]; }; then
+    cd "$DOT_DIR" || { echo "cd ${DOT_DIR} failed"; exit 1; }
+    if [ "$1" == "-d" ]; then
+        git diff
+    else
+        git add -i
+        read -rp "Commit message: " msg
+        git commit -m "$msg"
+        git push
+    fi
+fi
