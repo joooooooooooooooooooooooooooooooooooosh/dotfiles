b6b223a4a59c7b6ce50b515be79ba56d7f6cf800
Josh Harcombe <joshharc@gmail.com>
2025-02-12 16:25:48 +1100

    scratchpad

--- macos_scripts/scratchpad.sh
+++ macos_scripts/scratchpad.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/env bash
 
-scratchpad_id=$(yabai -m query --windows | jq '.[] | select(.title=="Scratchpad").id')
+scratchpad_id=$(aerospace list-windows --all | grep Scratchpad$ | cut -d' ' -f1)
 
 if [ -z "$scratchpad_id" ]; then
-  open -a Alacritty -n --args -t Scratchpad
-  sleep 0.1
-  scratchpad_id=$(yabai -m query --windows | jq '.[] | select(.title=="Scratchpad").id')
-  yabai -m window --focus "$scratchpad_id"
-  yabai -m window --resize abs:1920:1080
-  yabai -m window --move abs:960:540
+    open -a Alacritty -n --args -t Scratchpad
 else
-  current_space=$(yabai -m query --spaces --space | jq '.index')
-  read -r scratchpad_space is_minimized <<< \
-    "$(yabai -m query --windows --window "$scratchpad_id" | jq -j '.space," ", ."is-minimized"')"
-
-  if [[ "$is_minimized" = "true" ]] || [[ $current_space -ne $scratchpad_space ]]; then
-    yabai -m window "$scratchpad_id" --space "$current_space"
-    yabai -m window --focus "$scratchpad_id"
-  else
-    yabai -m window "$scratchpad_id" --minimize
-  fi
+    aerospace move-node-to-workspace --fail-if-noop --focus-follows-window --window-id "${scratchpad_id}" $(aerospace list-workspaces --focused) \
+        || aerospace move-node-to-workspace --window-id "${scratchpad_id}" scratchpad
 fi
