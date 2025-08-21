6cc82b7b771c8f472364c9e08c0aa8d192aa230c
Josh Harcombe <joshharc@gmail.com>
2025-08-22 09:47:11 +1000

    backup sketchybar workspace focus

--- config/sketchybar/plugins/aerospace.sh
+++ config/sketchybar/plugins/aerospace.sh
@@ -1,45 +1,54 @@
 #!/usr/bin/env sh
 
-if [ "${1}" = "${FOCUSED_WORKSPACE}" ]; then
-    sketchybar --set "${NAME}" background.drawing=on
-else
-    # When we navigate away from a space, check if it should still be on the bar
-    if [ "$(aerospace list-windows --workspace "${1}" --count)" -eq 0 ]; then
-        sketchybar --remove "${NAME}" event "aerospace_workspace_change_${1}"
-    fi
-
-    # Check if the new space has an element on the bar yet
-    if ! sketchybar --query "space.${FOCUSED_WORKSPACE}" >/dev/null 2>&1; then
-        direction="after"
-        clone_space=$({
-            aerospace list-workspaces --monitor all --empty no
-            echo "${FOCUSED_WORKSPACE}"
-        } | grep -xv scratchpad | sort -n | grep -x -B1 -m1 "${FOCUSED_WORKSPACE}" | sed '$d')
-
-        if [ -z "${clone_space}" ]; then
-            # New space is the first in the list
-            clone_space=$(aerospace list-workspaces --monitor all --empty no | head -1)
-            direction="before"
-        fi
-
-        sketchybar --add event "aerospace_workspace_change_${FOCUSED_WORKSPACE}"
-        sketchybar --clone "space.${FOCUSED_WORKSPACE}" "space.${clone_space}" "${direction}" \
-            --remove event "space.${FOCUSED_WORKSPACE}" "aerospace_workspace_change_${clone_space}" \
-            --subscribe "space.${FOCUSED_WORKSPACE}" "aerospace_workspace_change_${FOCUSED_WORKSPACE}" \
-            --set "space.${FOCUSED_WORKSPACE}" \
-            icon="${FOCUSED_WORKSPACE}" \
-            click_script="aerospace workspace ${FOCUSED_WORKSPACE}" \
-            script="plugins/aerospace.sh ${FOCUSED_WORKSPACE}" \
-            background.drawing=on
-    fi
-    sketchybar --set "${NAME}" background.drawing=off
-fi
-
 # Leave mouse.scrolled.global to the aerospace-mode module
 if [ "${SENDER}" = "mouse.scrolled" ]; then
     if echo "${SCROLL_DELTA}" | grep -q '-'; then
         aerospace list-workspaces --monitor focused --empty no | grep -v scratchpad | aerospace workspace --wrap-around prev
     else
         aerospace list-workspaces --monitor focused --empty no | grep -v scratchpad | aerospace workspace --wrap-around next
+    fi
+else
+    if [ "${1}" = "${FOCUSED_WORKSPACE}" ]; then
+        sketchybar --set "${NAME}" background.drawing=on
+    else
+        # When we navigate away from a space, check if it should still be on the bar
+        if [ "$(aerospace list-windows --workspace "${1}" --count)" -eq 0 ]; then
+            sketchybar --remove "${NAME}" event "aerospace_workspace_change_${1}"
+        fi
+
+        # Check if the new space has an element on the bar yet
+        if ! sketchybar --query "space.${FOCUSED_WORKSPACE}" >/dev/null 2>&1; then
+            direction="after"
+            clone_space=$({
+                aerospace list-workspaces --monitor all --empty no
+                echo "${FOCUSED_WORKSPACE}"
+            } | grep -xv scratchpad | sort -n | grep -x -B1 -m1 "${FOCUSED_WORKSPACE}" | sed '$d')
+
+            if [ -z "${clone_space}" ]; then
+                # New space is the first in the list
+                clone_space=$(aerospace list-workspaces --monitor all --empty no | head -1)
+                direction="before"
+            fi
+
+            sketchybar --add event "aerospace_workspace_change_${FOCUSED_WORKSPACE}"
+            sketchybar --clone "space.${FOCUSED_WORKSPACE}" "space.${clone_space}" "${direction}" \
+                --remove event "space.${FOCUSED_WORKSPACE}" "aerospace_workspace_change_${clone_space}" \
+                --subscribe "space.${FOCUSED_WORKSPACE}" "aerospace_workspace_change_${FOCUSED_WORKSPACE}" \
+                --set "space.${FOCUSED_WORKSPACE}" \
+                icon="${FOCUSED_WORKSPACE}" \
+                click_script="aerospace workspace ${FOCUSED_WORKSPACE}" \
+                script="plugins/aerospace.sh ${FOCUSED_WORKSPACE}" \
+                background.drawing=on
+        fi
+        sketchybar --set "${NAME}" background.drawing=off
+    fi
+
+    # Backup method for setting the focused workspace
+    # Sometimes apps steal focus multiple times, leaving all workspaces convinced they don't have focus
+    if ! aerospace list-workspaces --monitor focused --empty no |
+        xargs -I{} sketchybar --query space.{} 2>/dev/null |
+        jq -r .geometry.background.drawing |
+        grep -x on; then
+        sketchybar --set "space.$(aerospace list-workspaces --focused)" background.drawing=on
     fi
 fi
