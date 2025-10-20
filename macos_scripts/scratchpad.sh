953ce4ebd04b2b53f31dc738cd67c9f0ecb7e4d2
Josh Harcombe <joshharc@gmail.com>
2025-10-21 10:10:16 +1100

    lasdkjf

--- macos_scripts/scratchpad.sh
+++ macos_scripts/scratchpad.sh
@@ -5,6 +5,10 @@ scratchpad_id=$(aerospace list-windows --all | grep Scratchpad$ | cut -d' ' -f1)
 if [ -z "$scratchpad_id" ]; then
     open -a Alacritty -n --args -t Scratchpad
 else
-    aerospace move-node-to-workspace --fail-if-noop --focus-follows-window --window-id "${scratchpad_id}" $(aerospace list-workspaces --focused) ||
+    if aerospace move-node-to-workspace --fail-if-noop --focus-follows-window --window-id "${scratchpad_id}" "$(aerospace list-workspaces --focused)"; then
+        # Centre window using Rectangle shortcut
+        osascript -e 'tell application "System Events" to keystroke "c" using {command down, option down}'
+    else
         aerospace move-node-to-workspace --window-id "${scratchpad_id}" scratchpad
+    fi
 fi
